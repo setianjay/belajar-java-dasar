d51da9883513cfc3ddd5343073132a09850cfb3b
setianjay <hari.setiaji2000@gmail.com>
2021-07-09 00:31:35 +0700

    Initial commit. Type data boolean

---
+++ src/type_boolean/TypeBoolean.java
@@ -0,0 +1,14 @@
+package type_boolean;
+
+public class TypeBoolean {
+
+    public static void main(String[] args) {
+        boolean isConnect = true;
+
+        if (!isConnect){ // false
+            System.out.println("Try again");
+        }else{
+            System.out.println("Connect");
+        }
+    }
+}
