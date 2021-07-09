15acb99c7403a1a24a8108e9bf651ee8dc5fff18
setianjay <hari.setiaji2000@gmail.com>
2021-07-09 19:21:25 +0700

    Initial commit. Type data string

---
+++ src/type_string/TypeString.java
@@ -0,0 +1,14 @@
+package type_string;
+
+public class TypeString {
+
+    public static void main(String[] args) {
+        String firstName = "Hari";
+        String lastName = "Setiaji";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
