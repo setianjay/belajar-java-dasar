4d851fd6984d32268dbc9b2c0f7e3efcd8390395
setianjay <hari.setiaji2000@gmail.com>
2021-07-11 18:56:01 +0700

    Initial commit. Basic method

---
+++ src/method/BasicMethod.java
@@ -0,0 +1,12 @@
+package method;
+
+public class BasicMethod {
+
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+    public static void sayHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
